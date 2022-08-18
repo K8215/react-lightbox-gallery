0aa584f3f555b76917b5b5579c790c0edc151455
Kaitlyn Coleman <k8lak8@protonmail.com>
2022-08-18 15:48:13 -0400

    added react lightbox gallery project

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main className="global-main">
+      <div className="content-wrap" style={{backgroundImage: `url(./img/rex.jpg)`}}>
+        <div className="box box--animated">
+          <h1>Welcome</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sapiente dolor eum molestias ipsum numquam, asperiores suscipit quis ipsam consequatur repellat culpa? Blanditiis nostrum, deserunt harum explicabo sapiente magni. Ut culpa aut rem omnis est placeat doloremque labore a recusandae unde maiores temporibus ullam reprehenderit cum eum, minus ea. Dolor?</p>
+        </div>
+      </div>     
+    </main>
+  )
+}
+
+export default Home
